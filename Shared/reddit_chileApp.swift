ce21c44a3d11fc2ba2b2234f1c34c8ce9a9ad0c7
Byron Chavarría <byron.chavarria01@gmail.com>
2022-04-13 08:12:32 -0600

    Include base project

---
+++ Shared/reddit_chileApp.swift
@@ -0,0 +1,17 @@
+//
+//  reddit_chileApp.swift
+//  Shared
+//
+//  Created by Byron Chavarría on 13/4/22.
+//
+
+import SwiftUI
+
+@main
+struct reddit_chileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
